8627fa26c91953739b000d70895f11e04549f031
ria-leberu <gene.andrei.nadela@gmail.com>
2021-07-06 15:49:44 -0400

    added playbutton

--- app/src/main/java/com/example/leberuslabyrinth/SplashScreen.java
+++ app/src/main/java/com/example/leberuslabyrinth/SplashScreen.java
@@ -5,19 +5,76 @@ import android.graphics.BitmapFactory;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.view.View;
+import android.view.MotionEvent;
 
 public class SplashScreen extends View{
 
+    //title graphic setup
     private Bitmap titleG;
+    private int scrW; private int scrH;
+
+    //play button setup
+    private Bitmap playBtnUp;
+    private Bitmap playBtnDn;
+    private boolean playBtnPressed;
 
     public SplashScreen(Context context){
         super(context);
         titleG = BitmapFactory.decodeResource(getResources(), R.drawable.title_leberus_labyrinth);
+
+        playBtnUp = BitmapFactory.decodeResource(getResources(),R.drawable.playbutton_up_leberulabyrith);
+        playBtnDn = BitmapFactory.decodeResource(getResources(),R.drawable.playbutton_down_leberulabyrith);
+
+    }
+    @Override
+    public void onSizeChanged(int w, int h, int oldw, int oldh){
+        super.onSizeChanged(w, h, oldw, oldh);
+        scrW = w; scrH = h;
+    }
+
+    public boolean onTouchEven(MotionEvent event) {
+        int evtAction = event.getAction();
+        int X = (int)event.getX();
+        int Y = (int)event.getY();
+
+        switch (evtAction) {
+            case MotionEvent.ACTION_DOWN:
+                int btnLeft = (scrW - playBtnUp.getWidth())/2;
+                int btnRight = btnLeft + playBtnUp.getWidth();
+                int btnTop = (int) (scrH*0.5);
+                int btnBottom = btnTop + playBtnUp.getHeight();
+
+                boolean withinBtnBounds = X > btnLeft && X < btnRight && Y > btnTop && Y < btnBottom;
+                if(withinBtnBounds){
+                    playBtnPressed = true;
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                if(playBtnPressed) {
+                    //launch main game screen
+                }
+                playBtnPressed = false;
+                break;
+        }
+        invalidate();
+        return true;
     }
 
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawBitmap(titleG, 100, 100, null);
+
+        int titleGLeftPos = (scrW - titleG.getWidth())/2;
+        canvas.drawBitmap(titleG, titleGLeftPos, 100, null);
+
+        int playBtnLeftPos = (scrW -playBtnUp.getWidth())/2;
+        if (playBtnPressed) {
+            canvas.drawBitmap(playBtnDn, playBtnLeftPos, (int)(scrH * 0.5), null);
+        }
+        else {
+            canvas.drawBitmap(playBtnUp, playBtnLeftPos, (int)(scrH * 0.5), null);
+        }
     }
 
 }
